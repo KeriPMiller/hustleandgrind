c81155aad0c8dc0bdf6663bfe4e683cb29da3fa2
KeriPMiller <miller.keripatricia@gmail.com>
2018-05-11 16:36:56 -0400

    added calendar picker component

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
 import moment from "moment";
-import ReactTimeslotCalendar from "react-timeslot-calendar";
+import DayPicker from "react-day-picker";
+import "react-day-picker/lib/style.css";
+import { H1, Span } from "../assets/styledComponents";
 
-/* timeslot docs:
-*https://www.npmjs.com/package/react-timeslot-calendar
-*/
+let today = moment().format("MMM  Do, YYYY");
 
-const Calendar = () => {
-  let ignoreWeekends = {
-    "saturdays": false,
-    "sundays": false
-  };
-  return (
-    <div>
-      <ReactTimeslotCalendar
-        initialDate={moment().format()}
-        renderDays= {ignoreWeekends}
-      />
-    </div>
-  );
-};
+class Calendar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDay: undefined
+    };
+    this.handleDayClick = this.handleDayClick.bind(this);
+  }
+
+  handleDayClick(day, { selected }) {
+    if (selected) {
+      this.setState({ selectedDay: undefined });
+      return;
+    }
+    this.setState({ selectedDay: day });
+  }
+
+  render() {
+    return (
+      <div>
+        <H1>
+          Today is <Span color={"lawngreen"}>{today}</Span>
+        </H1>
+        <DayPicker
+          onDayClick={this.handleDayClick}
+          selectedDays={this.state.selectedDay}
+        />
+        {this.state.selectedDay ? (
+          <p>
+            You Selected {this.state.selectedDay.toLocaleDateString()}{" "}
+            {moment(
+              this.state.selectedDay.toISOString(),
+              "YYYY-MM-DD"
+            ).fromNow()}{" "}
+            from now.
+          </p>
+        ) : (
+          <p>Select a day to chat</p>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Calendar;
