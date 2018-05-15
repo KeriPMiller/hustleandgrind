f5e40b8048a596b48d50b7228dfe04505e22b712
KeriPMiller <miller.keripatricia@gmail.com>
2018-05-15 19:35:57 -0400

    added header and some style

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,43 +1,22 @@
-import React, { Component } from "react";
-import logo from "../assets/logo.svg";
-import styled from "react-emotion";
+import React, {Component} from "react";
 
 // imported Components
 import Calendar from "./Calendar";
+import Header from "./Header";
+
 // imported styled components
-import { H1, CenterDiv } from "../assets/styledComponents/";
-
-// styled components
-
-const Logo = styled("img")`
-  height: 80px;
-`;
-
-const Head = styled("div")`
-  background-color: rgb(81, 37, 22);
-  padding: 20px;
-  color: papayawhip;
-`;
-
-const Title = styled("h1")`
-  color: hotpink;
-  font-size: 1.5em;
-`;
+import {CenterDiv} from "../assets/styledComponents/";
 
 class App extends Component {
-  render() {
-    return (
-      <CenterDiv>
-        <Head>
-          <Logo src={logo} alt="logo" />
-          <Title>Hustle & Grind</Title>
-        </Head>
-        <H1>Hello World!</H1>
-        <p>An easier way to schedule coffee chats!</p>
-        <Calendar />
-      </CenterDiv>
-    );
-  }
+    render() {
+        return (
+            <CenterDiv>
+                <Header/>
+                <p>An easier way to schedule coffee chats!</p>
+                <Calendar/>
+            </CenterDiv>
+        );
+    }
 }
 
 export default App;
