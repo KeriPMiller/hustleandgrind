afacea4a55f38b0c55e23eb0a95df75ada9799d2
KeriPMiller <miller.keripatricia@gmail.com>
2018-05-21 21:47:22 -0400

    set up react-redux store

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 // import {Router} from 'react-router-dom';
-import store from ',/store';
+import store from './store';
 import './assets/styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
